fbc33338aaea0e1f89d5e0dbf6aa28f8ad6e376a
Yash091 <yvchaurasia11@gmail.com>
2022-04-02 00:52:44 +0530

    Fixed bugs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { Connection } from "./database/db.js";
 const app = express();
+
 app.use(
   cors({
     origin: ["http://localhost:3000"],
